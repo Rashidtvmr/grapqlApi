1591b4f7ce894203868817a3e85be195b79e6c33
akil98 <akilp.1998@gmail.com>
2019-05-12 07:04:15 +0530

    After adding page

--- App.js
+++ App.js
@@ -1,23 +1,18 @@
-const express=require('express');
-const fs=require('fs');
-const graphqlHttp=require('express-graphql');
-const mySchema=require('./schema');
-const myResolve=require('./resolver');
-const app=express();
-const PORT=process.env.port || 3000;
+const express = require("express");
+const fs = require("fs");
+const graphqlHttp = require("express-graphql");
+const mySchema = require("./schema");
+const myResolve = require("./resolver");
+const app = express();
+const PORT = process.env.PORT || 3000;
 app.listen(PORT);
 
-
-app.use('/graphql', graphqlHttp({
+app.use(express.static("public"));
+app.use(
+  "/graphql",
+  graphqlHttp({
     schema: mySchema,
     rootValue: myResolve,
-    graphiql: true,
-}));
-// app.get('/',(req,res,next)=>{
-//     res.render('');
-// });
-app.get('/', (req, res) => {
-    fs.readFile(__dirname + '/index.html', 'utf8', (err, text) => {
-        res.send(text);
-    });
-});
+    graphiql: true
+  })
+);
